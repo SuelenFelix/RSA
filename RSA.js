b518686f9ce20eb4f279a9fa9adf3ce15146381e
SuelenFelix <54537490+SuelenFelix@users.noreply.github.com>
2021-02-28 23:14:02 -0300

    Add files via upload

---
+++ RSA.js
@@ -0,0 +1,20 @@
+
+let p = 11;
+let q = 17;
+let n = (p*q);
+let z = (p-1) * (q-1);
+let d = 7;
+let e = 23; 
+
+
+function encrepitar(entrada){
+
+  return (Math.pow(entrada, e)  % N);
+
+}
+
+function desencripitar(){
+  
+  return (Math.pow(entrada, d)  % N);
+
+}
